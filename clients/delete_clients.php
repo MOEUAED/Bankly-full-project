a8b1c9375ea1f1a503243fef443a8513b421cf73
MOEUAED <ziyanimouad@gmail.com>
2025-12-19 17:26:36 +0100

    feat ( client ) : add PHP logic of delete client

---
+++ clients/delete_clients.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+require_once "../config.php";
+
+if (!isset($_GET['id'])) {
+    header("Location: list_clients.php");
+    exit;
+}
+
+$id = (int)$_GET['id'];
+
+$stmt = mysqli_prepare(
+    $connect,
+    "SELECT account_id FROM accounts WHERE customerid = ? LIMIT 1"
+);
+mysqli_stmt_bind_param($stmt, "i", $id);
+mysqli_stmt_execute($stmt);
+$res = mysqli_stmt_get_result($stmt);
+
+if (mysqli_num_rows($res) > 0) {
+    header("Location: list_clients.php?error=has_accounts");
+    exit;
+}
+
+$stmt = mysqli_prepare($connect, "DELETE FROM customers WHERE customer_id = ?");
+mysqli_stmt_bind_param($stmt, "i", $id);
+
+if (mysqli_stmt_execute($stmt)) {
+    header("Location: list_clients.php?msg=deleted");
+    exit;
+}
+
+header("Location: list_clients.php?error=failed");
+exit;
+?>
